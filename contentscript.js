1aae432a2273eda1a0766b33a7fe50fd62a7f9cf
Geza Kovacs <gkovacs@stanford.edu>
2016-12-21 18:21:56 -0800

    removed ineffective rule in content script

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,3 @@
 for (var x of document.querySelectorAll('link[type="application/opensearchdescription+xml"]')) {
   x.remove();
 }
-
-for (var x of document.querySelectorAll('input[type="search"]')) {
-  x.type = 'input';
-}
